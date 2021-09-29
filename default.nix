c16d93e69f355a1cb276f3929318224bd9930214
Gabriel Fontes <eu@misterio.me>
2021-09-29 07:42:39 -0300

    update flake lock

--- default.nix
+++ default.nix
@@ -9,12 +9,9 @@
 { pkgs ? import <nixpkgs> { } }:
 
 {
-  # The `lib`, `modules`, and `overlay` names are special
-  lib = import ./lib { inherit pkgs; }; # functions
-  modules = import ./modules; # NixOS modules
-  overlays = import ./overlays; # nixpkgs overlays
+  lib = import ./lib { inherit pkgs; };
+  modules = import ./modules;
+  overlays = import ./overlays;
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  argone = pkgs.callPackage ./pkgs/argone { };
 }
