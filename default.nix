8d34ae3a26d47765371f352fa0791c8428aaf3d2
Gabriel Fontes <eu@misterio.me>
2021-10-25 01:13:58 -0300

    add runescape launcher (from kira bruneau's repo)

--- default.nix
+++ default.nix
@@ -8,6 +8,7 @@
   comma = pkgs.callPackage ./pkgs/comma { };
   minicava = pkgs.callPackage ./pkgs/minicava { };
   rustlings = pkgs.callPackage ./pkgs/rustlings { };
+  runescape-launcher = pkgs.callPackage ./pkgs/runescape-launcher { };
   swayfader = pkgs.callPackage ./pkgs/swayfader { };
   argononed = pkgs.callPackage ./pkgs/argononed { };
 }
